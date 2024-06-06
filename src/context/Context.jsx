30b11242d96513e573e88ae4a594c3b2eaff71a9
XanderWorld <xander.mymails@gmail.com>
2024-06-06 17:42:18 +0200

    add typing effect/syntax

--- src/context/Context.jsx
+++ src/context/Context.jsx
@@ -12,6 +12,12 @@ const ContextProvider = (props) => {
     const [loading,setLoading] = useState(false);
     const [resultData, setResultData] = useState("");
 
+    const delayPara = (index, nextWord) => {
+        setTimeout(function (){
+            setResultData(prev=>prev+nextWord);
+        },75*index)
+    }
+
     const onSent = async (prompt) => {
 
         setRecentPrompt(input)
@@ -20,8 +26,26 @@ const ContextProvider = (props) => {
         setLoading(true)
         setShowResult(true)
 
+        // setPrevPrompts(prev=>[...prev, input])
         const response = await run(input)
-        setResultData(response)
+        let responseArray = response.split("**");
+        let newResponse;
+        for (let i=0; i<responseArray.length; i++){
+            if (i === 0 || i%2 !== 1){
+                newResponse += responseArray[i];
+            }else{
+                newResponse += "<b>"+responseArray[i]+"</b>";
+            }
+        }
+        let newResponse2 = newResponse.split("*").join("</br>")
+        let newResponseArray = newResponse2.split(" ");
+        for (let i=0; i<newResponseArray.length;i++){
+            const nextWord = newResponseArray[i];
+            delayPara(i,nextWord+" ")
+        }
+        // setResultData(newResponse2)
+        // setResultData(response)
+
 
         setLoading(false)
 
